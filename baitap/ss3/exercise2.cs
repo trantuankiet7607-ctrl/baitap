b44afe297fa6c69b145c8d2869daf36f1ef622fa
trantuankiet7607-ctrl <trantuankiet7607@gmail.com>
2026-08-27 20:37:53 +0700

    exercise2

--- baitap/ss3/exercise2.cs
+++ baitap/ss3/exercise2.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 using System.Reflection.PortableExecutable;
+using System.Runtime.ConstrainedExecution;
 using System.Runtime.InteropServices.Marshalling;
 using System.Security.Cryptography;
 using System.Transactions;
@@ -260,7 +262,122 @@ namespace baitap.ss3
                     Console.Write("Error");
                 }
             }
-            
+            static void bai6()
+            {
+                //                Tình huống thực tế: Bộ phận Nhân sự(HR) cần một công cụ xử lý dữ liệu thô nhập vào từ biểu mẫu đăng
+                //ký.Họ tên nhập vào thường bị lỗi thừa khoảng trắng, hoa thường lộn xộn.Cần chuẩn hóa tên và tạo tài
+                //khoản công ty.
+                //Kiến thức trọng tâm: Kiểu string, các phương thức Trim(), Split(), Substring(), ToLower(), ToUpper(),
+                //string.Join().
+                //Yêu cầu bài toán: 
+                //• Nhập vào một chuỗi họ tên thô từ bàn phím(Ví dụ: "   ngUYỄN   vĂn   aN   "). 
+                //• Loại bỏ khoảng trắng thừa ở đầu, cuối và giữa các từ(chỉ giữ lại 1 khoảng trắng giữa các từ).
+                //• Chuyển đổi chuỗi thành dạng Viết Hoa Chữ Cái Đầu Mỗi Từ(Title Case): "Nguyễn Văn An".
+                //• Tách thành Họ, Tên Đệm và Tên chính.
+                //• Tạo Username không dấu theo quy tắc: ten.hovatenm. (Ví dụ: an.nguyenvan).
+                //• Tạo Email công ty: username + "@company.edu.vn".
+                Console.Write("Nhap ten cua ban: ");
+                string ten0 = Console.ReadLine();
+                string ten1 = ten0.Trim();
+                string ten2 = ten1.ToLower();
+                string[] danhsach = ten2.Split(' ');
+                for (int i = 0; i < danhsach.Length; i++)
+                {
+                    string tuhientai = danhsach[i];
+                    if (tuhientai.Length > 0)
+                    {
+                        string chudau = tuhientai.Substring(0, 1).ToUpper();
+                        string conlai = tuhientai.Substring(1);
+                        danhsach[i] = chudau + conlai;
+
+                    }
+
+                }
+                string chuanhoa = string.Join(" ", danhsach);
+                string ho = danhsach[0];
+                string ten = danhsach[danhsach.Length - 1];
+
+                // --- CÁCH LẤY TÊN ĐỆM CHUẨN ---
+
+                // Tính số lượng từ đệm ở giữa
+                int soTuDem = danhsach.Length - 2;
+
+                // Tạo mảng mới để chứa các từ đệm
+                string[] mangTenDem = new string[soTuDem];
+
+                // Dùng for nhặt các từ ở giữa (từ index 1 đến trước chữ cuối cùng)
+                for (int i = 0; i < soTuDem; i++)
+                {
+                    mangTenDem[i] = danhsach[i + 1];
+                }
+                // Cuối cùng, dùng string.Join để dán các từ đệm lại
+                string tendem = string.Join(" ", mangTenDem);
+                Console.WriteLine($"Ten chuan  hoa:{chuanhoa}");
+                Console.WriteLine($"ho: {ho} | ten dem: {tendem} | ten: {ten}");
+                Console.WriteLine($"username: {ten.ToString().ToLower()}.{ho.ToString().ToLower()}{tendem.ToString().ToLower()}");
+                Console.WriteLine($"Email: {ten.ToString().ToLower()}.{ho.ToString().ToLower()}{tendem.ToString().ToLower()}@company.edu.vn");
+
+
+            }
+            static void bai7()
+            {
+                //                Bài 7: Lập Kế Hoạch Chi Phí Nhiên Liệu & Chia Sẻ Chuyến Đi(Car - pooling)
+                //Tình huống thực tế: Một nhóm bạn lên kế hoạch đi phượt bằng xe ô tô cá nhân. Họ cần một máy tính bỏ
+                //túi để ước tính tổng lượng nhiên liệu tiêu thụ, tổng chi phí xăng dầu và chia đều cho từng thành viên.
+                //Kiến thức trọng tâm: Kiểu double, decimal, int, Math.Ceiling, định dạng tiền tệ. 
+                //Yêu cầu bài toán: 
+                //• Nhập khoảng cách chuyến đi(km - kiểu double).
+                //• Nhập mức tiêu thụ nhiên liệu trung bình của xe(lít/ 100km - kiểu double). 
+                //• Nhập giá xăng hiện tại(VNĐ / lít - kiểu decimal).
+                //• Nhập số lượng người tham gia chuyến đi(người -kiểu int). 
+                //• Tính tổng số lít xăng cần dùng = (Quãng đường / 100) * Mức tiêu thụ. 
+                //• Tính tổng chi phí tiền xăng = Tổng số lít xăng * Giá xăng.
+                //• Tính số tiền mỗi người phải chi trả(làm tròn lên hàng nghìn VNĐ gần nhất bằng Math.Ceiling). 
+
+                Console.Write("Khoang cach chuyen di (km): ");
+                double s = double.Parse(Console.ReadLine());
+                Console.Write("muc tieu thu nhien lieu (lit/100km): ");
+                double h = double.Parse(Console.ReadLine());
+                Console.Write("Gia xang hien tai (VND/lit): ");
+                decimal t = decimal.Parse(Console.ReadLine());
+                Console.Write("So luong nguoi tham gia: ");
+                int n = int.Parse(Console.ReadLine());
+                Console.WriteLine($"Tong so lit xang can dung: {(s/100)*h}");
+                Console.WriteLine($"Tong tien xang: {(decimal)((s / 100) * h) * t}");
+                Console.WriteLine($"Moi nguoi phai tra: {Math.Ceiling((((decimal)((s / 100) * h) * t)/n)/1000)*1000}");
+            }
+            static void bai8()
+            {
+                Console.WriteLine("Ma OTP cua ban la: 839201");
+                DateTime thoigianguima = DateTime.Now;
+
+                Console.Write("Nhap ma OTP cua ban: ");
+                string chuoinhap = Console.ReadLine();
+                bool chinhxac = int.TryParse(chuoinhap, out int otp);
+                DateTime thoigiannhap = DateTime.Now;
+                if (chinhxac == true && otp == 839201 && thoigiannhap <= thoigianguima.AddMinutes(5))
+                {
+                    Console.WriteLine("Trang thai da duoc xac thuc - Phe duyet thanh cong");
+                    TimeSpan dt = thoigiannhap - thoigianguima;
+                    Console.WriteLine($"thoi gian nhap cua ban la: {dt}");
+                }
+                else if (chinhxac == false && thoigiannhap <= thoigianguima.AddMinutes(5))
+                {
+                    Console.WriteLine("Xac thuc khong thanh cong - Dinh dang khong hop le");
+                }
+                else if (chinhxac == true && otp != 839201 && thoigiannhap <= thoigianguima.AddMinutes(5))
+                {
+                    Console.WriteLine("Xac thuc khong thanh cong - Ma sai");
+                }
+                else
+                {
+                    Console.WriteLine("Xac thuc khong thanh cong - Qua thoi gian xac thuc");
+                }
+               
+            }
+            bai8();
+                  
+             
 
 
 
@@ -271,6 +388,8 @@ namespace baitap.ss3
 
 
 
+
+
+            }
         }
-    }
 }
